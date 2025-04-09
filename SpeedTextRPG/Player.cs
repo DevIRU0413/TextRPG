fb6782d94cc2b85b38a8fe193ce32fc3816a123d
DevIRU <deviru0413@gmail.com>
2025-04-09 20:22:54 +0900

    플레이어 수정
    플레이어 수정 및 필요 데이터 정의

--- SpeedTextRPG/Player.cs
+++ SpeedTextRPG/Player.cs
@@ -1,8 +1,40 @@
-﻿using System;
-
-public class Player
+﻿namespace SpeedTextRPG
 {
-	public Player()
-	{
-	}
+    public class Player
+    {
+        private readonly string _name; // 플레이어 파티의 이름
+        private int _haveGold; // 플레이어의 소지금
+
+        public CharacterGroup Group { get; private set; }
+
+        public Player(string playerName, string groupName, int haveGold = 0)
+        {
+            _name = playerName;
+            Group = new(groupName);
+            _haveGold = haveGold;
+        }
+
+        // 소지금 사용
+        public bool UseHaveGold(int useGold)
+        {
+            if (_haveGold < useGold) return false;
+
+            _haveGold -= useGold;
+            return true;
+        }
+        // 소지금 획득
+        public void GetGold(int getGold)
+        {
+            _haveGold += getGold;
+            if (_haveGold < 0)
+                _haveGold = 0;
+        }
+
+
+        // 몬스터와 충돌 시
+        public void BattleEncounter(CharacterGroup targetGroup)
+        {
+            BattleManager.Instance.Battle(Group, targetGroup);
+        }
+    }
 }
